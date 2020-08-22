42b02bae2fa4ec491f66d82f9d6c2ec4caeed251
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-22 20:13:08 +0530

    Attendance check

--- empwagejava.java
+++ empwagejava.java
@@ -1,5 +1,17 @@
-public class empwagejava{
-	public static void main(String[] args){
-	System.out.println("Welcome to Employee Wage Computaion Program.");
+import java.util.Random;
+public class empwagecompjava{
+	public static void main (String[] args){
+		System.out.println("Welcome to Employee Wage Computaion Program.");
+		int check = attendaceCheck();
+		if(check==1) {
+			System.out.println("Employee is present");
+		}
+		else {
+			System.out.println("Employee is absent");
+		}
+	}
+	public static int attendaceCheck() {
+		Random rand = new Random();
+		return rand.nextInt(2);
 	}
 }
